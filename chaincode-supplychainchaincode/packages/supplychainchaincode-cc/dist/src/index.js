6278b7e9a2d9e97bdf5290ae7bca843d07231836
mad-learner <37177023+mad-learner@users.noreply.github.com>
2019-11-17 16:54:48 +0530

    Add files via upload

---
+++ chaincode-supplychainchaincode/packages/supplychainchaincode-cc/dist/src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+tslib_1.__exportStar(require("./models/Customer.model"), exports);
+tslib_1.__exportStar(require("./models/Distributor.model"), exports);
+tslib_1.__exportStar(require("./models/Manufacturer.model"), exports);
+tslib_1.__exportStar(require("./models/Retailer.model"), exports);
+tslib_1.__exportStar(require("./models/Supplier.model"), exports);
+tslib_1.__exportStar(require("./supplychainchaincode.controller"), exports);
+//# sourceMappingURL=index.js.map
---
+++ chaincode-supplychainchaincode/packages/supplychainchaincode-cc/src/index.ts
@@ -0,0 +1,7 @@
+// Self generated file - anything in this file may be recreated by Convector-CLI
+export * from './models/Customer.model';
+export * from './models/Distributor.model';
+export * from './models/Manufacturer.model';
+export * from './models/Retailer.model';
+export * from './models/Supplier.model';
+export * from './supplychainchaincode.controller';
